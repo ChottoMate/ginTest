d762010fed4286bff8bab39c562f5bc9f069af72
ChottoMatte <jin92370@gmail.com>
2022-05-21 00:13:27 +0900

    お試し画面出力

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
-package ginTest
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+
+	router.GET("/", func(context *gin.Context) {
+		context.HTML(200, "index.html", gin.H{})
+	})
+
+	router.Run()
+}
